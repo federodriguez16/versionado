527de4cae60c408a2a16e56ac0a17aa3b803f43f
Federico Rodriguez Ferro <frodriguez@stechs.com.ar>
2025-10-21 16:50:59 -0300

    fix: Modifico nombres de Versions

--- internal/models/version.go
+++ internal/models/version.go
@@ -2,9 +2,9 @@ package models
 
 type Versions struct {
 	ID       uint `gorm:"primaryKey"`
-	productA string
-	productB string
-	productC string
-	productD string
-	productE string
+	ProductA string
+	ProductB string
+	ProductC string
+	ProductD string
+	ProductE string
 }
